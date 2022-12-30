079d38ae0e3012461f171e0271c64f987f116a78
myang5t3r <yangmatthew85@gmail.com>
2022-12-30 12:33:12 -0700

    String methods practice

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please Enter Your name");
+            string name = Console.ReadLine() ?? "";
+
+            Console.WriteLine(name.ToLower());
+            Console.WriteLine(name.ToUpper());
+            Console.WriteLine(name.Trim());
+            Console.WriteLine(name.Substring(3));
         }
     }
 }
