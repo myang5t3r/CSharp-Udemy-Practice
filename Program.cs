d702d6f77594d0ef1e4ad3c0150cf5e2111fdb2a
myang5t3r <yangmatthew85@gmail.com>
2022-12-31 09:50:11 -0700

    arrays and functions practice

--- Program.cs
+++ Program.cs
@@ -3,20 +3,33 @@
     class Program
     {
 
+        static void getOdd(Array arr)
+        {
+            // method should print every odd number 
+            foreach (int item in arr)
+            {
+                if (item % 2 != 0)
+                {
+                    System.Console.WriteLine(item);
+                }
+            }
+        }
+        static void getEven(Array arr)
+        {
+            // method should print every odd number 
+            foreach (int item in arr)
+            {
+                if (item % 2 == 0)
+                {
+                    System.Console.WriteLine(item);
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            string[] friends = { "Vanessa", "Josh", "Nic" };
-
-            foreach (var friend in friends)
-            {
-                GreetFriend(friend);
-            }
-
-        }
-
-        static void GreetFriend(string name)
-        {
-            Console.WriteLine($"Hi {name}, how are you?");
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            getOdd(array);
+            getEven(array);
         }
     }
 }
